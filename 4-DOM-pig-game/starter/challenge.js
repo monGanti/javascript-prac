63c1a652b1e0631800c733d42cc4f4f98433991d
Manisha Ganti <manishaganti@gmail.com>
2020-06-29 19:28:23 -0400

    completed chapter 4

--- 4-DOM-pig-game/starter/challenge.js
+++ 4-DOM-pig-game/starter/challenge.js
@@ -1,16 +1,14 @@
 /*
-GAME RULES:
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
 
-var scores, roundScore, activePlayer,dice,gamePlayer;
+var scores, roundScore, activePlayer,dice,gamePlayer,counterFor6;
 init();
 
 //what happens when player rolls dice
@@ -18,17 +16,34 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     if(gamePlayer){
         //1. random number generator
         dice = Math.floor(Math.random() * 6) + 1;
+        dice2 = Math.floor(Math.random() * 6) + 1;
 
         //2. display the dice result image
-        var diceDOM = document.querySelector('.dice');
+        var diceDOM = document.querySelector('.dice-0');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-'+dice+'.png';
+        var diceDOM2 = document.querySelector('.dice-1');
+        diceDOM2.style.display = 'block';
+        diceDOM2.src = 'dice-'+dice2+'.png';
 
         //3. update the base score if the rolled number is NOT 1
-        if (dice !== 1){
+        if (dice !== 1 && dice2 !== 1){
             //Add up the score
-            roundScore += dice;
+            roundScore += dice+dice2;
+            console.log(dice,dice2);
             document.querySelector('#current-'+activePlayer).textContent = roundScore;
+            
+            //also check if there are more than 2 times 6 is rolled then total score must turn back to 0
+            if (dice === 6 || dice2 ===6){
+                counterFor6 ++;
+                console.log('total active player 6s are' +counterFor6);
+                if(counterFor6 === 2){
+                    scores[activePlayer] = 0;
+                    document.querySelector('#current-'+activePlayer).textContent = '0';
+                    document.querySelector('#score-'+activePlayer).textContent = '0';
+                    nextPlayer();   
+                }
+            }
         } 
         else{
             nextPlayer();
@@ -46,10 +61,22 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
         //update the ui with new global score
         document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
 
+        //get the winning score value
+        var winningValue;
+        var value = document.querySelector('#winning-score-input').value;
+        console.log('the final number is:'+value);
+        // NOTE: All undefiend, null,0 or "" are all Type Coersed to false 
+        if(value){
+            winningValue = value;
+        }else{
+            winningValue = 100;
+        }
+
         //Check if player won the game
-        if(scores[activePlayer] >= 20){
+        if(scores[activePlayer] >= winningValue){
             document.querySelector('#name-'+activePlayer).textContent = 'Winner';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice-0').style.display = 'none';
+            document.querySelector('.dice-1').style.display = 'none';
             document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
             document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
             gamePlayer = false;
@@ -71,6 +98,7 @@ function nextPlayer(){
     //change the active player
     activePlayer === 0? activePlayer = 1:activePlayer =0;
     roundScore = 0;
+    counterFor6=0;
     //reset the score of both players back to 0
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
@@ -78,7 +106,8 @@ function nextPlayer(){
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     //do not display the dice image
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice-0').style.display = 'none';
+    document.querySelector('.dice-1').style.display = 'none';
 }
 
 //initial state of the game
@@ -87,6 +116,7 @@ function init(){
     roundScore = 0;
     activePlayer = 0;
     gamePlayer = true;
+    counterFor6 = 0;
 
     /**Practice only */
     /**dice = Math.floor(Math.random() * 6) + 1;
@@ -97,7 +127,8 @@ function init(){
     console.log(x);*/
 
     //hide the dice image
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice-0').style.display = 'none';
+    document.querySelector('.dice-1').style.display = 'none';
     // bring the GLobal Scores to 0
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
